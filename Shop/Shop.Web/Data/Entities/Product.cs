865eb5850745fc0f3059d57d9894306855e6eaad
Fabian Ordoñez <mmbtrs@gmail.com>
2020-03-14 15:03:49 -0500

    Se crea la clase ProductsController

---
+++ Shop/Shop.Web/Data/Entities/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "El campo {0} solo puede tener un máximo de {1} caracteres.")]
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Imagen")]
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "Última compra")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? LastPurchase { get; set; }
+
+        [Display(Name = "Última venta")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? LastSale { get; set; }
+
+        [Display(Name = "Está disponible?")]
+        public bool IsAvailabe { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+
+        //public User User { get; set; }
+
+        //public string ImageFullPath
+        //{
+        //    get
+        //    {
+        //        if (string.IsNullOrEmpty(this.ImageUrl))
+        //        {
+        //            return null;
+        //        }
+
+        //        return $"https://shopzulu.azurewebsites.net{this.ImageUrl.Substring(1)}";
+        //    }
+        //}
+    }
+}
